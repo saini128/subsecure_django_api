f3bac9e94da1526d0e670201d9e7ddf0caec8957
saini128 <saini.hck@gmail.com>
2024-10-20 01:22:19 +0530

    changes in consumer.py

--- workers/consumers.py
+++ workers/consumers.py
@@ -1,76 +1,61 @@
-# workers/consumers.py
 import json
+import asyncio
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
 
 class WorkerConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         await self.accept()
+        self.stream_task = asyncio.create_task(self.stream_workers())
 
     async def disconnect(self, close_code):
-        pass
+        
+        self.stream_task.cancel()
 
-    async def receive(self, text_data):
-        # Import Worker model here
-        from .models import Worker
-        # workers = Worker.objects.all().values('name', 'age', 'location_description')
-        # await self.send(text_data=json.dumps({
-        #     'workers': list(workers)
-        # }))
-        # workers = await database_sync_to_async(self.get_workers)()
-        # await self.send(text_data=json.dumps({
-        #     'workers': workers
-        # }))
-        print("Receiving data:", text_data)
-        data = json.loads(text_data)
-    # Check if the action is to get workers
-        # if data.get('action') == 'get_workers':
-        #     print("Fetching workers...")
-        # Fetch workers from the database asynchronously
-        workers = await self.get_workers()
-        print("Workers fetched:", workers)
-        await self.send(text_data=json.dumps({
-                'workers': workers
-        }))
+    async def stream_workers(self):
+        while True:
+            try:
+                
+                workers = await self.get_workers()
+                await self.send(text_data=json.dumps({
+                    'workers': workers
+                }))
+                
+                await asyncio.sleep(5)  
+            except asyncio.CancelledError:
+                
+                break
+
     @database_sync_to_async
     def get_workers(self):
         from .models import Worker
-        # This runs in a synchronous context
+        
         return list(Worker.objects.select_related('location').values('name', 'age', 'location__description'))
-        #return list(Worker.objects.values('name', 'age'))
-  
-
-    
 class LocationConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         await self.accept()
+        self.stream_task = asyncio.create_task(self.stream_locations())
 
     async def disconnect(self, close_code):
-        pass
+        
+        self.stream_task.cancel()
 
-    async def receive(self, text_data):
-        # Import Location model here
-        from workers.models import Location
-        print("Receiving data:", text_data)
-        data = json.loads(text_data)
-        # locations = Location.objects.all().values('location_description', 'temperature', 'O2_level')
-        # await self.send(text_data=json.dumps({
-        #     'locations': list(locations)
-        # }))
-        # locations = await database_sync_to_async(self.get_locations)()
-        # await self.send(text_data=json.dumps({
-        #     'locations': locations
-        # }))
-        locations = await self.get_locations()
-        print("Locations fetched:", locations)
-
-            # Send the locations back to the WebSocket
-        await self.send(text_data=json.dumps({
-                'locations': locations
-            }))
+    async def stream_locations(self):
+        while True:
+            try:
+                
+                locations = await self.get_locations()
+                await self.send(text_data=json.dumps({
+                    'locations': locations
+                }))
+                
+                await asyncio.sleep(5)  
+            except asyncio.CancelledError:
+                
+                break
 
     @database_sync_to_async
     def get_locations(self):
         from workers.models import Location
-        # This runs in a synchronous context
-        return list(Location.objects.values('description', 'temperature', 'o2_level'))
+        
+        return list(Location.objects.values('ldescription', 'temperature', 'O2_level'))
