f343a77440a443e432cdf249d1ff43344d9b44a6
saini128 <saini.hck@gmail.com>
2024-11-25 05:00:28 +0530

    models recievers implemented properly

--- workers/models.py
+++ workers/models.py
@@ -1,5 +1,6 @@
+from concurrent.futures.thread import _worker
 from django.db import models
-from django.db.models.signals import post_save, pre_delete
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from django.contrib.auth import get_user_model
 from django.core.validators import MinValueValidator
@@ -51,7 +52,6 @@ class SafeLevels(models.Model):
     
     def __str__(self):
         return "Safe Levels"
-
 class Worker(models.Model):
     id = models.CharField(max_length=10, primary_key=True)  
     name = models.CharField(max_length=100)  
@@ -59,49 +59,61 @@ class Worker(models.Model):
     location = models.ForeignKey(Location, on_delete=models.SET_NULL, null=True, blank=True, related_name='workers')  
     sos = models.BooleanField(default=False)  
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._original_location_id = self.location_id if self.location_id else None
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self._original_location_id = self.location_id
+
     def delete(self, *args, **kwargs):
-        
-        if self.location:
-            self.location.number_of_workers -= 1
-            self.location.save()
         super().delete(*args, **kwargs)
 
     def __str__(self):
         return self.name
-    
-@receiver(post_save, sender=Location)
-def update_emergency_bit(sender, instance, **kwargs):
-    
-    safe_levels = SafeLevels.objects.first()
-    if not safe_levels:
-        return  
 
-    
-    emergency = (
-        (instance.temperature is not None and (instance.temperature < safe_levels.temperature_min or instance.temperature > safe_levels.temperature_max)) or
-        (instance.humidity is not None and (instance.humidity < safe_levels.humidity_min or instance.humidity > safe_levels.humidity_max)) or
-        (instance.pm10_level is not None and (instance.pm10_level < safe_levels.pm10_min or instance.pm10_level > safe_levels.pm10_max)) or
-        (instance.pm25_level is not None and (instance.pm25_level < safe_levels.pm25_min or instance.pm25_level > safe_levels.pm25_max)) or
-        (instance.pm1_level is not None and (instance.pm1_level < safe_levels.pm1_min or instance.pm1_level > safe_levels.pm1_max)) or
-        (instance.o2_level is not None and (instance.o2_level < safe_levels.o2_min or instance.o2_level > safe_levels.o2_max))
-    )
-
-    
-    if instance.emergency_bit != emergency:
-        instance.emergency_bit = emergency
-        instance.save(update_fields=["emergency_bit"])
 
 @receiver(post_save, sender=Worker)
-def update_worker_count_on_save(sender, instance, **kwargs):
-    if instance.location:
-        location = instance.location
-        location.number_of_workers = location.workers.count()
-        location.save(update_fields=["number_of_workers"])
+def update_worker_counts_on_save(sender, instance, **kwargs):
+    """
+    Updates worker counts when a worker is saved, handling both old and new locations
+    """
+    # If the worker's location has changed, update both old and new locations
+    if kwargs.get('update_fields') != frozenset(['location']):  # Prevent recursive saves
+        # Update new location's count
+        if instance.location:
+            instance.location.number_of_workers = instance.location.workers.count()
+            instance.location.save(update_fields=['number_of_workers'])
+        
+        # If this is an update (not a new worker), also update the old location's count
+        if not kwargs.get('created', False) and hasattr(instance, '_original_location_id'):
+            old_location_id = instance._original_location_id
+            if old_location_id and old_location_id != instance.location_id:
+                try:
+                    old_location = Location.objects.get(id=old_location_id)
+                    old_location.number_of_workers = old_location.workers.count()
+                    old_location.save(update_fields=['number_of_workers'])
+                except Location.DoesNotExist:
+                    pass
 
-
-@receiver(pre_delete, sender=Worker)
-def update_worker_count_on_delete(sender, instance, **kwargs):
+@receiver(post_delete, sender=Worker)
+def update_location_count_on_worker_delete(sender, instance, **kwargs):
+    """
+    Updates the location's worker count after a worker is deleted
+    """
     if instance.location:
-        location = instance.location
-        location.number_of_workers = location.workers.count() - 1  # Reduce count by 1
-        location.save(update_fields=["number_of_workers"])
+        instance.location.number_of_workers = instance.location.workers.count()
+        instance.location.save(update_fields=['number_of_workers'])
+
+def update_all_location_counts():
+    """
+    Utility function to update all location worker counts
+    """
+    for location in Location.objects.all():
+        worker_count = location.workers.count()
+        if location.number_of_workers != worker_count:
+            location.number_of_workers = worker_count
+            location.save(update_fields=['number_of_workers'])
+
+# Add save method to Worker model to track location changes
